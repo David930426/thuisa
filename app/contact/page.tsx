7dd17da30da7fc941e9016f43949842eb56a5f21
timothy <timothygunawanwisno@gmail.com>
2025-08-14 17:55:36 +0700

    add contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,19 +1,79 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 export default function Page() {
   return (
     <div className="min-h-screen">
-      <h1 className="text-xl md:text-5xl font-bold mb-4 md:my-20 text-red-600 text-center">
-        OUR CONTACTS
-      </h1>
-      <div className="text-center my-10">
-        <h1 className="text-red-600 md:text-3xl font-bold">ASK THUISA</h1>
-        <h3>Contact Our Line:</h3>
-        <p>lineId thuisa</p>
-        <h3>Find Our Latest Update:</h3>
-        <p>Tiktok: </p>
-        <p>Instagram: </p>
-        <h3>Email Us: </h3>
-        <p>thuisa2025@gmail.com</p>
+      <div className="w-full text-white bg-red-600 text-center py-15 md:py-25">
+        <h1 className="text-4xl md:text-7xl font-bold pb-2 md:pb-4">
+          Contact THUISA
+        </h1>
+        <p className="md:text-2xl text-md">
+          Have questions or need support? <br />
+          Contact us, we are happy to help
+        </p>
       </div>
+      <div className="md:max-w-5xl mx-10 md:mx-auto mt-10 md:mt-20 md:flex gap-10 text-center mb-10">
+        <div className="md:w-1/2 px-10 md:pb-20 pb-10 pt-10 rounded-3xl shadow-md">
+          <h1 className="text-red-600 md:text-5xl text-3xl font-bold mb-10">
+            Contacts
+          </h1>
+          <h3 className="text-red-600 md:text-2xl text-xl mb-1">
+            Contact Our Line:
+          </h3>
+          <p className="mb-5 md:mb-8 md:text-xl text-md">lineId thuisa</p>
+          <h3 className="text-red-600 md:text-2xl text-xl mb-1">
+            Find Our Latest Update:
+          </h3>
+          <p className="md:text-xl text-md">Tiktok: @thuisa_official</p>
+          <p className="mb-5 md:mb-8 md:text-xl text-md">
+            Instagram: thuisa_official
+          </p>
+          <h3 className="text-red-600 md:text-2xl text-xl mb-1">Email Us: </h3>
+          <p className="md:text-xl text-md">thuisa2025@gmail.com</p>
+        </div>
+        <div className="md:w-1/2 rounded-3xl shadow-md px-10 md:pb-20 pb-10 pt-10">
+          <h1 className="text-red-600 md:text-5xl text-3xl font-bold mb-7">
+            Our Location:
+          </h1>
+          <div>
+            <iframe
+              title="Tunghai Map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3639.693668951489!2d120.59997247606017!3d24.182473772050418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693e1c3a406707%3A0xd43d01bc4fb2806a!2sUniversitas%20Tunghai!5e0!3m2!1sid!2sid!4v1755167301397!5m2!1sid!2sid"
+              allowFullScreen
+              className="w-full md:min-h-100 min-h-75"
+            ></iframe>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-4xl md:text-7xl font-bold pb-2 md:pb-4 text-center text-red-600 md:mt-30 mt-20">
+        Ask THUISA
+      </h1>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="question1">
+          <AccordionTrigger>
+            <p className="max-w-5xl mx-10 md:mx-auto flex">Test</p>
+          </AccordionTrigger>
+          <AccordionContent>
+            <p className="max-w-5xl mx-10 md:mx-auto flex">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+              tincidunt dictum libero non lacinia. Nunc suscipit congue lacus,
+              in egestas arcu. Morbi mollis dui turpis, sit amet venenatis enim
+              aliquet at. Aliquam metus enim, tempus nec dui quis, tempor tempor
+              ipsum. Quisque massa diam, ullamcorper vel suscipit eget, ultrices
+              a lacus. In vel quam eu sapien vulputate semper id vel eros. Nam
+              fringilla elementum turpis quis ultricies. Quisque id mi lacus.
+              Fusce a ultricies tortor, vel semper dolor. Curabitur eget lacus
+              eros. Mauris imperdiet vestibulum sapien, id laoreet justo pretium
+              a. Sed malesuada vestibulum tempus.
+            </p>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
