ca0b31260ccf17115bc74efac41d155f93718d7a
timothy <timothygunawanwisno@gmail.com>
2025-08-18 11:56:34 +0700

    minor fix

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -3,7 +3,13 @@ import ComingSoon from "@/components/coming-soon";
 export default function Page() {
   return (
     <div className="w-full min-h-screen">
-      <ComingSoon />
+      <div className="w-full bg-red-600 text-white text-center py-15 md:py-25">
+        <h1 className="text-4xl md:text-6xl font-bold">Our Events</h1>
+      </div>
+      <h1>Upcoming Events</h1>
+      <div className="shadow-md rounded-2xl max-w-6xl mx-auto p-10">
+        <h1>YINGXIN 1</h1>
+      </div>
     </div>
   );
 }
