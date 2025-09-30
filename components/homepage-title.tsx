6518701bfbe06d1afc8b9a4571360b87e908429a
David <davidgunawanwisno@gmail.com>
2025-09-30 15:36:44 +0800

    change in the homepage title

---
+++ components/homepage-title.tsx
@@ -0,0 +1,10 @@
+export default function Title() {
+  return (
+    <div className="w-full bg-red-600 text-center mb-20 py-20 text-white px-10">
+      <h1 className="text-5xl md:text-9xl font-bold mb-3">THUISA</h1>
+      <p className="text-xl md:text-3xl">
+        Tunghai Univeristy Indonesian Student Association
+      </p>
+    </div>
+  );
+}
--- components/homepage.tsx
+++ components/homepage.tsx
@@ -1,16 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import { ButtonPrimary, ButtonSecondary } from "./ui/button";
+import { ButtonPrimary, ButtonSecondary } from "@/components/ui/button";
+import Title from "@/components/homepage-title";
 
 export default function Homepage() {
   return (
     <>
-      <div className="w-full bg-red-600 text-center mb-20 py-20 text-white px-10">
-        <h1 className="text-5xl md:text-9xl font-bold mb-3">THUISA</h1>
-        <p className="text-xl md:text-3xl">
-          Tunghai Univeristy Indonesian Student Association
-        </p>
-      </div>
+      <Title />
       <div className="max-w-5xl mx-10 md:mx-auto mt-20 flex">
         <div>
           <Image
