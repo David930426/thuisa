bc44fa31c35b0a7122b75fd0fc37fb24e2f86ee0
David Gunawan W <147725115+David930426@users.noreply.github.com>
2025-10-01 23:33:05 +0800

    fix the dialog

--- components/event-dialog.tsx
+++ components/event-dialog.tsx
@@ -21,106 +21,112 @@ export default function EventDialog({ event }: { event: EventData }) {
           View Event
         </ButtonPrimary>
       </DialogTrigger>
-      <DialogContent className="2xl:min-w-6xl md:min-w-3xl max-h-[85vh] overflow-y-auto md:text-xl text-md">
-        <DialogHeader>
-          <DialogTitle className="text-red-600 md:text-3xl uppercase">
-            {event.name}
-          </DialogTitle>
-          <DialogDescription className="md:text-xl text-md">
-            {event.subInfo ? event.subInfo : "THUISA Event"}
-          </DialogDescription>
-        </DialogHeader>
-        <Tabs defaultValue="about">
-          <TabsList className="md:py-6 md:my-3 md:mb-5 my-2">
-            <TabsTrigger
-              value="about"
-              className="md:text-xl md:p-5 hover:cursor-pointer text-md"
-            >
-              About
-            </TabsTrigger>
-            <TabsTrigger
-              value="ticket"
-              className="md:text-xl md:p-5 hover:cursor-pointer text-md"
-              disabled={event.noTicket}
-            >
-              Ticketing
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="about" className="space-y-5">
-            <p>
-              <span className="font-bold">Event Type: </span>
-              {event.type ? event.type : "TBD"}
-            </p>
-            <p>
-              <span className="font-bold">Who Must Attend: </span>
-              {event.who ? event.who : "TBD"}
-            </p>
-            <p className="capitalize">
-              <span className="font-bold">Date & Time: </span>
-              {!(!event.day && !event.month && !event.year)
-                ? `${event.day ? event.day : ""} ${event.month} ${event.year}`
-                : "TBD"}
-            </p>
-            <p>
-              <span className="font-bold">Location: </span>
-              {event.location ? event.location : "TBD"}
-            </p>
-            {event.about && (
-              <>
-                <h1 className="font-bold">About the Event</h1>
-                <p>{event.about}</p>
-              </>
-            )}
-            {event.activities && (
-              <>
-                <h1 className="font-bold">Activities</h1>
-                {event.activities.length > 1 ? (
-                  <ul className="list-disc list-inside">
-                    {event.activities.map((e, index) => (
-                      <li key={index}>{e}</li>
-                    ))}
-                  </ul>
-                ) : (
-                  <p>{event.activities}</p>
-                )}
-              </>
-            )}
-            {event.reasons && (
-              <>
-                <h1 className="font-bold">Why you should join</h1>
-                {event.reasons.length > 1 ? (
-                  <ul className="list-disc list-inside">
-                    {event.reasons.map((e, index) => (
-                      <li key={index}>{e}</li>
-                    ))}
-                  </ul>
-                ) : (
-                  <p>{event.reasons}</p>
-                )}
-              </>
-            )}
-            {event.note && (
-              <>
-                <h1 className="font-bold">Important Note</h1>
-                <p>{event.note}</p>
-              </>
-            )}
-            <DialogFooter className="md:mt-15 mt-10">
-              <DialogClose asChild>
-                <ButtonPrimary>Close</ButtonPrimary>
-              </DialogClose>
-            </DialogFooter>
-          </TabsContent>
-          <TabsContent value="ticket">
-            <ComingSoon />
-            <DialogFooter className="md:mt-15 mt-10">
-              <DialogClose asChild>
-                <ButtonPrimary>Close</ButtonPrimary>
-              </DialogClose>
-            </DialogFooter>
-          </TabsContent>
-        </Tabs>
-      </DialogContent>
+      <EventDialogContent event={event} />
     </Dialog>
+  );
+}
+
+export function EventDialogContent({ event }: { event: EventData }) {
+  return (
+    <DialogContent className="2xl:min-w-6xl md:min-w-3xl max-h-[85vh] overflow-y-auto md:text-xl text-md">
+      <DialogHeader>
+        <DialogTitle className="text-red-600 md:text-3xl uppercase">
+          {event.name}
+        </DialogTitle>
+        <DialogDescription className="md:text-xl text-md">
+          {event.subInfo ? event.subInfo : "THUISA Event"}
+        </DialogDescription>
+      </DialogHeader>
+      <Tabs defaultValue="about">
+        <TabsList className="md:py-6 md:my-3 md:mb-5 my-2">
+          <TabsTrigger
+            value="about"
+            className="md:text-xl md:p-5 hover:cursor-pointer text-md"
+          >
+            About
+          </TabsTrigger>
+          <TabsTrigger
+            value="ticket"
+            className="md:text-xl md:p-5 hover:cursor-pointer text-md"
+            disabled={event.noTicket}
+          >
+            Ticketing
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="about" className="space-y-5">
+          <p>
+            <span className="font-bold">Event Type: </span>
+            {event.type ? event.type : "TBD"}
+          </p>
+          <p>
+            <span className="font-bold">Who Must Attend: </span>
+            {event.who ? event.who : "TBD"}
+          </p>
+          <p className="capitalize">
+            <span className="font-bold">Date & Time: </span>
+            {!(!event.day && !event.month && !event.year)
+              ? `${event.day ? event.day : ""} ${event.month} ${event.year}`
+              : "TBD"}
+          </p>
+          <p>
+            <span className="font-bold">Location: </span>
+            {event.location ? event.location : "TBD"}
+          </p>
+          {event.about && (
+            <>
+              <h1 className="font-bold">About the Event</h1>
+              <p>{event.about}</p>
+            </>
+          )}
+          {event.activities && (
+            <>
+              <h1 className="font-bold">Activities</h1>
+              {event.activities.length > 1 ? (
+                <ul className="list-disc list-inside">
+                  {event.activities.map((e, index) => (
+                    <li key={index}>{e}</li>
+                  ))}
+                </ul>
+              ) : (
+                <p>{event.activities}</p>
+              )}
+            </>
+          )}
+          {event.reasons && (
+            <>
+              <h1 className="font-bold">Why you should join</h1>
+              {event.reasons.length > 1 ? (
+                <ul className="list-disc list-inside">
+                  {event.reasons.map((e, index) => (
+                    <li key={index}>{e}</li>
+                  ))}
+                </ul>
+              ) : (
+                <p>{event.reasons}</p>
+              )}
+            </>
+          )}
+          {event.note && (
+            <>
+              <h1 className="font-bold">Important Note</h1>
+              <p>{event.note}</p>
+            </>
+          )}
+          <DialogFooter className="md:mt-15 mt-10">
+            <DialogClose asChild>
+              <ButtonPrimary>Close</ButtonPrimary>
+            </DialogClose>
+          </DialogFooter>
+        </TabsContent>
+        <TabsContent value="ticket">
+          <ComingSoon />
+          <DialogFooter className="md:mt-15 mt-10">
+            <DialogClose asChild>
+              <ButtonPrimary>Close</ButtonPrimary>
+            </DialogClose>
+          </DialogFooter>
+        </TabsContent>
+      </Tabs>
+    </DialogContent>
   );
 }
--- components/homepage-events.tsx
+++ components/homepage-events.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
 import { eventData } from "@/lib/data";
-import EventDialog from "./event-dialog";
+import { EventDialogContent } from "./event-dialog";
+import { Dialog, DialogTrigger } from "./ui/dialog";
 
 export default function Events() {
   const events = [
@@ -25,33 +26,41 @@ export default function Events() {
       <Title />
       <div className="max-w-7xl md:mx-auto py-10 md:py-10 md:px-20 px-5 md:shadow rounded-4xl">
         <div className="md:flex">
-          {events.map((event) => (
-            <div
-              className="w-full border-gray border-1 p-5 md:p-6 rounded-4xl shadow mr-10 max-md:mb-5"
-              key={event.name}
-            >
-              <h1 className="text-center text-xl md:text-2xl font-bold mb-4 md:mb-8 text-red-600 uppercase">
-                {event.name}
-              </h1>
-              <div className="flex">
-                <div className="md:mr-5 mr-3">
-                  <Image
-                    src={`./${event.img}`}
-                    alt={event.name}
-                    width={200}
-                    height={220}
-                    className="rounded-xl md:min-w-50 mr-6 min-w-30"
-                  />
-                </div>
-                <div className="md:mt-5">
-                  <p>Date: {event.date ? event.date : "TBD"} </p>
-                  <p>Location: {event.location ? event.location : "TBD"}</p>
-                  <p className="md:mt-5 md:mb-7">{event.info}</p>
-                  <LearnMore eventName={event.name} />
-                </div>
-              </div>
-            </div>
-          ))}
+          {events.map((event) => {
+            const selected = eventData.find(
+              (select) => select.name === event.name
+            );
+            return selected ? (
+              <Dialog key={event.name}>
+                <DialogTrigger asChild>
+                  <div className="w-full border-gray border-1 p-5 md:p-6 rounded-4xl shadow mr-10 max-md:mb-5 hover:shadow-xl hover:cursor-pointer">
+                    <h1 className="text-center text-xl md:text-3xl font-bold mb-4 md:mb-8 text-red-600 uppercase">
+                      {event.name}
+                    </h1>
+                    <div className="flex">
+                      <div className="md:mr-5 mr-3">
+                        <Image
+                          src={`./${event.img}`}
+                          alt={event.name}
+                          width={200}
+                          height={220}
+                          className="rounded-xl md:min-w-50 mr-6 min-w-30"
+                        />
+                      </div>
+                      <div className="md:mt-5 md:text-xl">
+                        <p>Date: {event.date ? event.date : "TBD"} </p>
+                        <p>
+                          Location: {event.location ? event.location : "TBD"}
+                        </p>
+                        <p className="md:mt-5 md:mb-7 mt-3">{event.info}</p>
+                      </div>
+                    </div>
+                  </div>
+                </DialogTrigger>
+                <EventDialogContent event={selected} />
+              </Dialog>
+            ) : null;
+          })}
         </div>
         <Link
           href="/events"
@@ -61,13 +70,6 @@ export default function Events() {
         </Link>
       </div>
     </>
-  );
-}
-
-function LearnMore({ eventName }: { eventName: string }) {
-  const selectedData = eventData.find((select) => select.name === eventName);
-  return (
-    selectedData && <EventDialog event={selectedData} />
   );
 }
 
